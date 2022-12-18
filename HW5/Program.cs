cd47af074d68c9228d92a937504e34667ee48197
AndreiS <rd1.gr@yandex.ru>
2022-12-18 23:09:11 +0700

    soh

---
+++ HW5/Program.cs
@@ -0,0 +1,144 @@
+﻿// Задача 34: Задайте массив заполненный случайными положительными 
+// трёхзначными числами. Напишите программу, которая покажет количество 
+// чётных чисел в массиве.
+// [345, 897, 568, 234] -> 2
+
+// int size = EvenInt("Введите размерность массива -> ");
+// int [] numbers = new int[size];
+// FillArrayRandomNumbers(numbers);
+// PrintArray(numbers);
+// int result = 0;
+
+// for (int i = 0; i < numbers.Length; i++)
+// {
+//     if (numbers[i] % 2 == 0)
+//     {
+//         result++;
+//     }
+// }
+// if (result % 10 == 1)
+// {
+//     Console.WriteLine($"В массиве {result} четное число");
+// }
+// if (result % 10 == 2 || result % 10 == 3 || result % 10 == 4)
+// {
+//     Console.WriteLine($"В массиве {result} четных числа");
+// }
+
+// void FillArrayRandomNumbers(int [] array) 
+// {
+//     for (int i = 0; i < array.Length; i++)
+//     {
+//         array[i] = new Random().Next(99, 999);
+//     }
+// }
+
+// void PrintArray(int[] array) 
+// {
+//     for (int i = 0; i < array.Length; i++)
+//     {
+//         Console.Write(array[i] + " ");
+//     }
+//     Console.WriteLine();
+// }
+//  int EvenInt(string message)  
+// {
+//      Console.Write(message);
+//      return Convert.ToInt32(Console.ReadLine());
+// }
+
+// Задача 36: Задайте одномерный массив, заполненный случайными числами. 
+// Найдите сумму элементов, стоящих на нечётных позициях.
+// [3, 7, 23, 12] -> 19
+// [-4, -6, 89, 6] -> 0
+
+// int size = ReadInt("Введите размер массива -> ");
+// int min = ReadInt("Введите min число массива -> ");
+// int max = ReadInt("Введите max число массива -> ");
+// int [] numbers = new int[size];
+// FillArrayRandomNumbers(numbers);
+// PrintArray(numbers);
+// int result = 0;
+
+// for (int i = 0; i < numbers.Length; i++)
+// {
+//     if (i % 2 != 0)
+//     {
+//         result = result + numbers[i];
+//     }
+// }
+// Console.WriteLine($"Сумма  нечетных позиций -> {result}");
+
+// void FillArrayRandomNumbers(int [] array) 
+// {
+//     for (int i = 0; i < array.Length; i++)
+//     {
+//         array[i] = new Random().Next(min, max);
+//     }
+// }
+
+// void PrintArray(int[] array) 
+// {
+//     for (int i = 0; i < array.Length; i++)
+//     {
+//         Console.Write(array[i] + " ");
+//     }
+//     Console.WriteLine();
+// }
+
+// int ReadInt(string message)  
+// {
+//     Console.Write(message);
+//     return Convert.ToInt32(Console.ReadLine());
+// }
+
+// Задача 38: Задайте массив вещественных чисел. Найдите разницу между
+// максимальным и минимальным элементов массива.
+// [3 7 22 2 78] -> 76
+
+int size = ReadInt("Введите размер массива -> ");
+int min = ReadInt("Введите min число массива -> ");
+int max = ReadInt("Введите max число массива -> ");
+int [] numbers = new int[size];
+
+FillArrayRandomNumbers(numbers);
+PrintArray(numbers);
+
+int maxFillarray = numbers[0];
+int minFillarray = numbers[0];
+
+for (int i = 0; i < numbers.Length; i++)
+{
+    if (numbers[i] > maxFillarray)
+    {
+        maxFillarray = numbers[i];
+    }
+    if (numbers[i] < minFillarray)
+    {
+        minFillarray = numbers[i];
+    }
+}
+Console.WriteLine($"Разница между max и min числом = {maxFillarray - minFillarray}");
+
+void FillArrayRandomNumbers(int [] array) 
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(min, max);
+    }
+}
+
+void PrintArray(int[] array) 
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+    Console.WriteLine();
+}
+
+int ReadInt(string message)  
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
---
+++ HW9/Program.cs
@@ -0,0 +1,91 @@
+﻿// Задача 64: Задайте значение N. Напишите программу, которая выведет все натуральные числа 
+// в промежутке от N до 1. Выполнить с помощью рекурсии.
+
+// N = 5 -> "5, 4, 3, 2, 1"
+// N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+
+// void NaturalNumbers(int N, int count)
+// {
+//     if (count > N)
+//     {
+//         return;
+//     }
+//     else
+//     {
+//         NaturalNumbers(N, count + 1);
+//         Console.Write(count + " ");
+//     }
+// }
+
+// Console.Write("Введите число: ");
+// int N = Convert.ToInt32(Console.ReadLine());
+// int count = 1;
+// NaturalNumbers(N, count);
+
+
+// void ShowNumbers(int n)
+// {
+//     if(n>=1)
+//     {
+//         ShowNumbers(n-1);  
+//         Console.Write(n + " ");
+        
+//     }
+// }
+// ShowNumbers(10);
+
+// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму 
+// натуральных элементов в промежутке от M до N.
+
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+// void NumberSum (int M, int N, int sum)
+// {
+//     if (M > N) 
+//     {
+//         Console.WriteLine($"Сумма натуральных элементов в промежутке от M до N: {sum}"); 
+//         return;
+//     }
+//     sum = sum + (M++);
+//     NumberSum(M, N, sum);
+// }
+
+// Console.WriteLine("Введите начало промежутка  ");
+// int M = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите конец промежутка  ");
+// int N = Convert.ToInt32(Console.ReadLine());
+// NumberSum(M, N, 0);
+
+
+// Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны 
+// два неотрицательных числа m и n.
+// m = 2, n = 3 -> A(m,n) = 9
+// m = 3, n = 2 -> A(m,n) = 29
+
+void AkkermanFunction(int m, int n)
+{
+    Console.Write(Akkerman (m, n)); 
+}
+
+int Akkerman(int m, int n)
+{
+    if (m == 0)
+    {
+        return n + 1;
+    }
+    else if (n == 0 && m > 0)
+    {
+        return Akkerman(m - 1, 1);
+    }
+    else
+    {
+        return (Akkerman(m - 1, Akkerman(m, n - 1)));
+    }
+}
+
+Console.Write("Введите число M: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+AkkermanFunction(m,n);
